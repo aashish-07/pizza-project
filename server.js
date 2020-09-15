f1f342f9f61ac6efdafa1f26f10269dfb9e174cf
Roman Empire <iaashish07@gmail.com>
2020-09-15 12:52:58 +0530

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3000
+
+
+// Create Server
+app.get("/", (req, res) => {
+    res.render('home')
+})
+
+// Set Template Engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
