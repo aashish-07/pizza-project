3b6cddcb6b4eb411cd8c9d7b3fc0627949326d4d
Roman Empire <iaashish07@gmail.com>
2020-09-17 14:38:32 +0530

    add cart,auth

--- server.js
+++ server.js
@@ -5,18 +5,29 @@ const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
-// Assets 
-app.use(express.static('public'))
-// Create Server
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
 // Set Template Engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
+// Assets 
+app.use(express.static('public'))
+
+// Create Server
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
