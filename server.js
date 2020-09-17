80ce9aaf215a2687bb20d0cbefdfdea9b548006d
Roman Empire <iaashish07@gmail.com>
2020-09-17 21:42:22 +0530

    add to cart

--- server.js
+++ server.js
@@ -1,33 +1,67 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const session  = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
+
+// DB Connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
+
+// Session store 
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+
+// Session Config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24} // 24 hour 
+}))
 
 // Set Template Engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
+app.use(flash())
+
 // Assets 
 app.use(express.static('public'))
+app.use(express.json())
+
+// Global middleware 
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    res.locals.user = req.user
+    next()
+})
 
 // Create Server
-app.get('/', (req, res) => {
-    res.render('home')
-})
+require('./routes/web')(app)
 
-app.get('/cart', (req, res) => {
-    res.render('customers/cart')
-})
 
-app.get('/login', (req, res) => {
-    res.render('auth/login')
-})
-app.get('/register', (req, res) => {
-    res.render('auth/register')
-})
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
